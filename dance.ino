79b1ac703b52584d8ceaf77183c8571b75ceb3fb
Gstars <103547660+Gstars01@users.noreply.github.com>
2024-11-03 13:46:13 +0900

    Update dance.ino
    change another code

--- dance.ino
+++ dance.ino
@@ -17,19 +17,76 @@ void loop() {
   spinRight(400);
   spinLeft(400);
   spinRight(400);
+  spinLeft(1800);
+  stop(300);
+  spinLeft(200);
+  spinRight(200);
+  spinLeft(200);
+  spinRight(200);
+  stop(100);
+
+  spinLeft(400);
+  spinRight(400);
+  spinLeft(400);
+  spinRight(400);
+  spinLeft(400);
+  spinRight(400);
+  spinLeft(400);
+  spinRight(400);
+  spinRight(2000);
+  spinLeft(200);
+  spinRight(400);
+  spinLeft(600);
+
+  moveForward(1600);
+  spinLeft(1600);
+  moveForward(1600);
+  spinRight(1600);
+
+  moveForward(400);
+  moveBackward(400);
+  moveForward(400);
+  moveBackward(400);
   spinLeft(1600);
 
-  moveForward(400);
   moveBackward(400);
   moveForward(400);
   moveBackward(400);
-  spinLeft(1700);
+  moveForward(400);
+  spinRight(1600);
 
-  moveBackward(400);
-  moveForward(400);
-  moveBackward(400);
-  moveForward(400);
-  spinRight(1700);
+  spinLeft(400);
+  spinRight(400);
+  spinLeft(400);
+  spinRight(400);
+  spinLeft(400);
+  spinRight(400);
+  spinLeft(400);
+  spinRight(400);
+  spinLeft(1800);
+  stop(300);
+  spinLeft(200);
+  spinRight(200);
+  spinLeft(200);
+  spinRight(200);
+  stop(100);
+
+  spinLeft(400);
+  spinRight(400);
+  spinLeft(400);
+  spinRight(400);
+  spinLeft(400);
+  spinRight(400);
+  spinLeft(400);
+  spinRight(400);
+  spinLeft(1800);
+  stop(300);
+  spinLeft(200);
+  spinRight(200);
+  spinLeft(200);
+  spinRight(200);
+  stop(10000);
+  
 }
 
 void moveForward(int duration) {
@@ -53,5 +110,11 @@ void spinLeft(int duration) {
 void spinRight(int duration) {
   rightServo.write(1700);
   leftServo.write(1700);
+  delay(duration);
+}
+
+void stop(int duration) {
+  rightServo.write(1500);
+  leftServo.write(1500);
   delay(duration);
 }
